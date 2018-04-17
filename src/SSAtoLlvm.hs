3d544323d2244103ed1c1e64d321ed346bc284ac
zehnpaard <zehnpaard@googlemail.com>
2018-04-17 09:13:38 +0900

    Add correct start/end section to SSAtoLLVM compiler

--- src/SSAtoLlvm.hs
+++ src/SSAtoLlvm.hs
@@ -9,5 +9,5 @@ ssaToLlvm ["store", s, n] = "store i32 " ++ n ++ ", i32* %" ++ s ++ "\n"
 ssaToLlvm ["alloc", s] = "%" ++ s ++ " = alloca i32" ++ "\n"
 ssaToLlvm other = (concat $ map show other) ++ "\n"
 
-start = ""
-end = ""
+start = " @.str = private unnamed_addr constant [4 x i8] c\"%d\\0A\\00\"\n\ndefine i32 @lispfn(){"
+end = "ret i32 %a1\n}\n\ndefine i32 @main() {\n%call = call i32 @lispfn()\n%call1 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %call)\nret i32 0\n}\n\ndeclare i32 @printf(i8*, ...)\n"
